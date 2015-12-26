ceb8f67a2d836b41b1a96a2c857368e57440565c
ErazerBrecht <brecht.carlier@student.ap.be>
2015-12-26 22:55:16 +0100

    Added some comments in the code.

--- Arduino/PlayMusic/PlayMusic/PlayMusic.ino
+++ Arduino/PlayMusic/PlayMusic/PlayMusic.ino
@@ -23,11 +23,13 @@
 #include <Audio.h>
 
 void setup() {
-  // debug output at 9600 baud
-  Serial.begin(9600);
+  //Start UART @ 115200 baud rate
+  Serial.begin(115200);
 
-  // setup SD-card
+  //Setup SD-card
   Serial.print("Initializing SD card...");
+  //"Start" SD Card
+  //Pin 4 is the CS pin
   if (!SD.begin(4)) {
     Serial.println(" failed!");
     return;
@@ -38,12 +40,12 @@ void setup() {
 
   //Mono: 44100Khz 
   //Stereo: 88200 sample rate
-  //300 mSec of prebuffering.
-  Audio.begin(1 * 44100, 300);
+  //100 mSec of prebuffering.
+  //1.3 => Play it faster => 30%
+  Audio.begin(1.3 * 44100, 100);
 }
 
 void loop() {
-  int count = 0;
 
   // open wave file from sdcard
   File myFile = SD.open("test.wav");
@@ -56,27 +58,21 @@ void loop() {
   const int S = 1024; // Number of samples to read in block
   short buffer[S];
 
-  Serial.print("Playing");
-  // until the file is not finished
+  Serial.println("Playing");
+  //Until the file is not finished
   while (myFile.available()) {
-    // read from the file into buffer
+    //Read from the file into buffer
     myFile.read(buffer, sizeof(buffer));
-
-    // Prepare samples
-    int volume = 1024;
-    Audio.prepare(buffer, S, volume);
-    // Feed samples to audio
+    //Prepare samples, save them in the buffer
+    //S = Numver of samples
+    //1024 = Volume   
+    Audio.prepare(buffer, S, 1024);
+    //Feed samples to audio, read buffer and write it on the DAC => Sound!!!
     Audio.write(buffer, S);
-
-    // Every 100 block print a '.'
-    count++;
-    if (count == 100) {
-      Serial.print(".");
-      count = 0;
-    }
   }
+  
   myFile.close();
 
   Serial.println("End of file. Thank you for listening!");
-  while (true) ;
+  while (true);   //Infinite loop, program stopped!
 }
