cadac6377122239a1174942c8b6da22f317c823e
Brecht Carlier <erazerbrecht@gmail.com>
2016-01-09 21:38:35 +0100

    Update PlayMusic.ino
    More comments

--- Arduino/PlayMusic/PlayMusic.ino
+++ Arduino/PlayMusic/PlayMusic.ino
@@ -4,20 +4,24 @@
   Demonstrates the use of the Audio library for the Arduino Due
 
   Hardware required :
-   Arduino shield with a SD card on CS4
-   A sound file named "test.wav" in the root directory of the SD card
-   An audio amplifier to connect to the DAC0 and ground
-   A speaker to connect to the audio amplifier
+  SD card reader with CS on pin 4
+  A sound file named "test.wav" in the root directory of the SD card
+  An audio amplifier to connect to the DAC0 and ground
+  A speaker to connect to the audio amplifier
 
   Original by Massimo Banzi September 20, 2012
   Modified by Scott Fitzgerald October 19, 2012
+  Changed for NightCoreThis by Brecht Carlier January 6, 2016
 
   This example code is in the public domain
-
   http://www.arduino.cc/en/Tutorial/SimpleAudioPlayer
 
 */
 
+//Include libraries
+//SD => Reading SD card
+//SPI => Communicate with SD card
+//Audio => Playing WAV files on DAC
 #include <SD.h>
 #include <SPI.h>
 #include <Audio.h>
@@ -28,7 +32,7 @@ void setup() {
 
   //Setup SD-card
   Serial.print("Initializing SD card...");
-  //"Start" SD Card
+  //"Start" SD Card => SPI intitializing, ...
   //Pin 4 is the CS pin
   if (!SD.begin(4)) {
     Serial.println(" failed!");
@@ -47,16 +51,16 @@ void setup() {
 
 void loop() {
 
-  // open wave file from sdcard
-  File myFile = SD.open("test2.wav");
+  //Open wave file from sdcard
+  File myFile = SD.open("test.wav");
   if (!myFile) {
-    // if the file didn't open, print an error and stop
-    Serial.println("error opening test.wav");
+    //If the file didn't open, print an error and stop
+    Serial.println("Error opening test.wav");
     while (true);
   }
 
-  const int S = 1024; // Number of samples to read in block
-  short buffer[S];
+  const int S = 1024; //Number of samples to read in block
+  short buffer[S];    //Init buffer
 
   Serial.println("Playing");
   //Until the file is not finished
@@ -67,7 +71,7 @@ void loop() {
     //S = Numver of samples
     //1024 = Volume   
     Audio.prepare(buffer, S, 1024);
-    //Feed samples to audio, read buffer and write it on the DAC => Sound!!!
+    //Feed samples to audio, read S amount of samples from the buffer and write it on the DAC buffer => Sound!!!
     Audio.write(buffer, S);
   }
   
